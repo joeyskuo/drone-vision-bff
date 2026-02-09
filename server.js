04dec149edfaab827ad5ef42e5b1853ffdef8507
Joey Kuo <joeyskuo@gmail.com>
2026-02-08 19:25:57 -0800

    server.js replaces index.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const detectRoutes = require('./src/routes/detect');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/detect', detectRoutes);
+
+module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
