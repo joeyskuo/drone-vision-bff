6143bb84993eb31f5d854903555dd890fe570e68
Joey Kuo <joeyskuo@gmail.com>
2026-02-11 19:15:16 -0800

    warmup route makes health check to api

--- src/routes/warmup.js
+++ src/routes/warmup.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Warmup Started');
+router.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(process.env.DRONE_API_ENDPOINT, {
+            headers: {
+                'X-API-Key': process.env.X_API_KEY
+            }
+        });
+    
+        res.json(response.data);
+    
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
 });
 
 module.exports = router;
